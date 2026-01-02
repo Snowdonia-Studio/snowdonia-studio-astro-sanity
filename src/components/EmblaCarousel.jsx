2790fe43e45a9f388e7739b48e4407ab8f147ec8
Shannon Grice <shannonbgrice@gmail.com>
2026-01-02 15:29:29 -0500

    FIX: update footer, hero banner, services, and process sections. Added dasiyui and used in services section

--- src/components/EmblaCarousel.jsx
+++ src/components/EmblaCarousel.jsx
@@ -3,8 +3,8 @@ import useEmblaCarousel from 'embla-carousel-react';
 
 function EmblaSlide({ title, description }) {
   return (
-    <div className="embla__slide flex-[0_0_100%] md:flex-[0_0_33.3333%] min-w-0 bg-teal-200 rounded-2xl h-40 flex flex-col items-start justify-center text-black p-6 shadow-lg">
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
+    <div className="embla__slide flex-[0_0_100%] md:flex-[0_0_33.3333%] min-w-0 bg-teal-200 rounded-2xl flex flex-col text-black p-8 shadow-lg">
+      <h2 className="text-2xl mb-2">{title}</h2>
       <p className="text-base">{description}</p>
     </div>
   );
--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ const TEXT = {
     'LETS_CHAT': "Let's Chat",
     'OUR_PROCESS': "Our Process",
     'OUR_THOUGHTS': "Our Thoughts",
+    'OUR_WORK': "Our Work",
+    'OUR_SERVICES': "Our Services",
     'READ_ARTICLE': "Read Article",
 }
 
