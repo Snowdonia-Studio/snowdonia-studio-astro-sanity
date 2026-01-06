9367a06c4a5dd87b47dc4f8b4328a093a5e23937
Jeremy <jeremy@Jeremys-MacBook-Pro.local>
2026-01-05 20:19:44 -0500

    Configure Netlify adapter for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify';
 
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
